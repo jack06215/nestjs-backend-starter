dba06b8958b9b81c0a7fe4706429f22603bfad19
Jack Cho <jack06215@gmail.com>
2024-01-28 12:14:36 +0900

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Configure api doc
+  const config = new DocumentBuilder()
+    .setTitle('Nestjs Clean Architecture Server')
+    .addTag('Hello', 'Hello API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(8080);
 }
 bootstrap();
--- src/presentation/controllers/hello.controller.ts
+++ src/presentation/controllers/hello.controller.ts
@@ -1,10 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
 import { SayHelloUseCase } from 'src/usecase/say-hello-world-usecase';
 
+@ApiTags('Hello')
 @Controller()
 export class HelloController {
-  constructor(private readonly sayHelloUseCase: SayHelloUseCase) {}
+  constructor(private readonly sayHelloUseCase: SayHelloUseCase) { }
 
+  @ApiCreatedResponse({
+    description: 'Return Hello World',
+  })
   @Get()
   getHello(): string {
     return this.sayHelloUseCase.execute();
