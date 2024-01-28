ae35e1f57358011cf8b6f9862e09cc806b588cbe
Jack Cho <jack06215@gmail.com>
2024-01-28 22:24:55 +0900

    add migration sample

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from '@mikro-orm/postgresql';
+import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
+import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
+import { Migrator } from '@mikro-orm/migrations';
+import { EntityGenerator } from '@mikro-orm/entity-generator';
+import { SeedManager } from '@mikro-orm/seeder';
+
+export default defineConfig({
+  host: 'localhost',
+  port: 5432,
+  user: 'postgres',
+  password: 'postgres',
+  dbName: 'postgres',
+  entities: ['dist/**/*.entity.js'],
+  entitiesTs: ['src/**/*.entity.ts'],
+  debug: true,
+  highlighter: new SqlHighlighter(),
+  metadataProvider: TsMorphMetadataProvider,
+  extensions: [Migrator, EntityGenerator, SeedManager],
+});
